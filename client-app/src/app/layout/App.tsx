8ba8925df87fddec4976a70f34ddf1beb3398a42
kevinnz2020 <kevinduongla@gmail.com>
2022-04-30 22:09:42 +1200

    end of section 7

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,113 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect} from 'react';
 import { Container } from 'semantic-ui-react';
-import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import {v4 as uuid} from 'uuid';
-import agent from '../api/agent';
+
+
 import LoadingComponent from './LoadingComponent';
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 
 function App() {
-  //const [activities, setActivities] = useState([]);
-  const [activities, setActivities] = useState<Activity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-  const [editMode, setEditMode] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const [submitting, setSubmitting] = useState(false);
+// video 70 : use MoBX
+  const {activityStore} = useStore();
 
-  function handleSelectActivity(id: string) {
-    setSelectedActivity(activities.find(x => x.id === id));
-  }
-
-  function handleCancelSelectActivity() {
-    setSelectedActivity(undefined);
-  }
-
-  function handleFormOpen(id?: string) {
-    id ? handleSelectActivity(id) : handleCancelSelectActivity();
-    setEditMode(true);
-  }
-  function handleFormClose() {
-    setEditMode(false);
-  }
-
-  function handleCreateOrEditActivity(activity: Activity) {
-    setSubmitting(true);
-    if (activity.id) {
-        agent.Activities.update(activity).then(() => {
-          setActivities([...activities.filter(x => x.id !== activity.id), activity])    
-          setSelectedActivity(activity);
-          setEditMode(false);
-          setSubmitting(false);
-        })
-    } else {
-      activity.id = uuid();
-      agent.Activities.create(activity).then(() => {
-          setActivities([...activities, activity])
-          setSelectedActivity(activity);
-          setEditMode(false);
-          setSubmitting(false);
-      })
-    }
-  }
-  function handleDeleteActivity(id: string) {
-    setSubmitting(true);
-    agent.Activities.delete(id).then(() => {
-      setActivities([...activities.filter(x => x.id !== id )])
-      setSubmitting(false);
-
-    })
-    
-  }
- 
-  /* useEffect(() => {
-    axios.get('http://localhost:5000/api/activities').then(response => {
-      setActivities(response.data);
-    })
-  },[]) */
-
-  /* video 62
   useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
-      setActivities(response.data);
-    })
-  },[])
-*/
-useEffect(() => {
-  agent.Activities.list().then(response => {
-    
-    // this is similar to View Model
-    let activities: Activity[] = [];
+ activityStore.loadActivities();
+},[activityStore])
 
-    response.forEach(activity => {
-      activity.date = activity.date.split('T')[0];
-      /// add record to VM after update the date format
-      activities.push(activity);
-    })
-    setActivities(activities);
-    setLoading(false);
-  })
-},[])
+if (activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
 
-if (loading) return <LoadingComponent content='Loading app'/>
   return (
     <>
-      <NavBar openForm={handleFormOpen} />
+      <NavBar/>
       <Container style={{marginTop: '7em'}}>
-        <ActivityDashboard activities={activities}
-          selectedActivity = {selectedActivity}
-          selectActivity = {handleSelectActivity}
-          cancelSelectActivity = {handleCancelSelectActivity}
-          editMode={editMode}
-          openForm={handleFormOpen}
-          closeForm={handleFormClose}
-          createOrEdit={handleCreateOrEditActivity}
-          deleteActivity={handleDeleteActivity}
-          submitting={submitting}
-        />
+        <ActivityDashboard />
         </Container>      
     </>
   );
 }
 
-export default App;
+export default observer(App);
--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { Button, Container, Menu } from "semantic-ui-react";
+import { useStore } from "../stores/store";
 
+
+/*
+refactored by video 74
 interface Props {
     openForm: () => void;
 }
-export default function NavBar({openForm}: Props) {
+*/
+
+export default function NavBar() {
+    
+    const {activityStore} = useStore();
+
     return (
         <Menu inverted fixed='top'>
             <Container>
@@ -14,7 +23,7 @@ export default function NavBar({openForm}: Props) {
                 </Menu.Item>
                 <Menu.Item name="Activities"/>
                 <Menu.Item header>
-                  <Button onClick={openForm} positive content='Create Activity'/>
+                  <Button onClick={() => activityStore.openForm()} positive content='Create Activity'/>
                 </Menu.Item>
             </Container>
 
